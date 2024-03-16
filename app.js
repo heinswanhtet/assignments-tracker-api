6615faf2f1bc6cf8467dd4908ab7eef3f05d4f29
Hein Swan Htet <hshscientist@gmail.com>
2024-03-16 14:34:50 +0630

    Installed security packages and added to the app

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 
+// security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 // database
 const connectDB = require('./db/connection')
 
@@ -16,9 +22,19 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleWare = require('./middleware/error-handler')
 const authenticateUser = require('./middleware/authentication')
 
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000,
+        max: 60
+    })
+)
+app.use(helmet())
+app.use(xss())
+app.use(cors())
+
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('Welcome! This is the assignments tracker.'))
+app.get('/', (req, res) => res.send('<h1>Welcome! This is the assignments tracker.</h1>'))
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/assignments', authenticateUser, assignmentsRouter)
