d30e15de10f90cc8e503e73ec457f7a786ae0397
Hein Swan Htet <hshscientist@gmail.com>
2024-03-16 10:36:32 +0630

    Added login functionality logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,7 +9,23 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    res.send('This is login')
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        throw new BadRequestError('Please provide email and password to login')
+    }
+
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+    const isPasswordCorrect = await user.comparePassword(password)
+    if (!isPasswordCorrect) {
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 }
 
 module.exports = {
