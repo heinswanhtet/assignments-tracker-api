03e5c52f8b25ef9101a2f043b18dd0a448110271
Hein Swan Htet <hshscientist@gmail.com>
2024-03-22 21:23:44 +0630

    Added query functionality

--- controllers/assignments.js
+++ controllers/assignments.js
@@ -3,8 +3,64 @@ const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 
 const getAllAssignments = async (req, res) => {
-    const assignments = await Assignment.find({ createdBy: req.user.userId }).sort('createdAt')
-    res.status(StatusCodes.OK).json({ total: assignments.length, assignments })
+    const { title, subject, instructor, status, sort } = req.query
+
+    const queryObject = {
+        createdBy: req.user.userId
+    }
+
+    if (title) {
+        queryObject.title = { $regex: title, $options: 'i' }
+    }
+
+    if (subject) {
+        queryObject.subject = { $regex: subject, $options: 'i' }
+    }
+
+    if (instructor) {
+        queryObject.instructor = { $regex: instructor, $options: 'i' }
+    }
+
+    if (status && status !== 'all') {
+        queryObject.status = status
+    }
+
+    let result = Assignment.find(queryObject)
+
+    if (sort === 'latest') {
+        result = result.sort('-createdAt')
+    }
+
+    if (sort === 'oldest') {
+        result = result.sort('createdAt')
+    }
+
+    if (sort === 'a-z') {
+        result = result.sort('title')
+    }
+
+    if (sort === 'z-a') {
+        result = result.sort('-title')
+    }
+
+    // limiting & pagination
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    result = result.skip(skip).limit(limit)
+
+    const assignments = await result
+
+    const totalAssignments = await Assignment.countDocuments(queryObject)
+
+    res.status(StatusCodes.OK).json({
+        total: totalAssignments,
+        total_in_current_page: assignments.length,
+        page,
+        hasNextPage: totalAssignments > (page * limit) ? true : false,
+        assignments,
+    })
 }
 
 const getAssignment = async (req, res) => {
